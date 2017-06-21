305df5004fcc20ddc9e7f5792bdeee7c3fb4cea4
theor <theo.richart@gmail.com>
2017-06-21 01:22:49 -0400

    Tone types

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as Tone from 'tone';
+
+var polySynth = new Tone.PolySynth(4, () => new Tone.Synth());
+polySynth.toMaster();
+Tone.Transport.start('+0.1');
+polySynth.triggerAttackRelease('C2', '8n', '@1m', 2);
 
 ReactDOM.render(
   <App />,
